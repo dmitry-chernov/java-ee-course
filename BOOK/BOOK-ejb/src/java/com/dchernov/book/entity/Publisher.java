c655643b900183f4eca6cd5f7b07f0d757d9b18c
Dmitry Chernov <ch_spb_ru@yahoo.com>
2015-11-21 15:31:33 +0200

    webservice -> Queue->MDB -> EJB -> persist/merge

--- BOOK/BOOK-ejb/src/java/com/dchernov/book/entity/Publisher.java
+++ BOOK/BOOK-ejb/src/java/com/dchernov/book/entity/Publisher.java
@@ -12,24 +12,24 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 /**
  *
  * @author Dc
  */
-@Entity
-@Table(name = "publisher")
+@Entity(name = "Publisher")
+@Table(name = "publisher", uniqueConstraints = {
+    @UniqueConstraint(name = "IDENTITY_KEY", columnNames = {"name", "city"})})
 public class Publisher implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name="city")
+    @Column(name = "city")
     private String city;
-
-    @Column(name="name")
+    @Column(name = "name")
     private String name;
 
     public Long getId() {
@@ -54,26 +54,6 @@ public class Publisher implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Publisher)) {
-            return false;
-        }
-        Publisher other = (Publisher) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
     }
 
     @Override
